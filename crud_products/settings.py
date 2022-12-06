f4ecb445e8925d829c06392c18ceef87307afbd6
kydiazpe <kdiazped@emeal.nttdata.com>
2022-12-06 16:15:17 -0500

    ajuste en el nombre de la base de datos

--- crud_products/settings.py
+++ crud_products/settings.py
@@ -130,7 +130,7 @@ WSGI_APPLICATION = 'crud_products.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'tienda',
     }
 }
 
